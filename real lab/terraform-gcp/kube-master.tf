240c8d63f1264aaa0599ca15fb333316563208c9
Cachac <carlos.echc11@gmail.com>
2020-10-17 18:30:43 -0600

    rke-master

--- real lab/terraform-gcp/kube-master.tf
+++ real lab/terraform-gcp/kube-master.tf
@@ -17,31 +17,163 @@
 # terraform plan
 # terraform apply
 
-provider "google" {
-  project = "kubernetes-292714"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+# check: provisionin Rancher cluster: https://medium.com/@chfrank_cgn/building-a-rancher-cluster-on-google-cloud-with-terraform-31f1453fbb31
+# rke GCP: https://rancher.com/docs/rancher/v2.x/en/quick-start-guide/deployment/google-gcp-qs/
+# GCP terraform resourses: https://www.terraform.io/docs/providers/google/r/compute_address.html
+
+# ssh keys
+resource "tls_private_key" "global_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "kubemaster"
-  machine_type = "f1-micro"
+resource "local_file" "ssh_private_key_pem" {
+  filename          = "${path.module}/id_rsa"
+  sensitive_content = tls_private_key.global_key.private_key_pem
+  file_permission   = "0600"
+}
+
+# Networking
+resource "google_compute_network" "rke_network" {
+  name = "rke-network"
+}
+
+resource "google_compute_subnetwork" "rke_subnet" {
+  name          = "rke-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.gcp_region
+  network       = google_compute_network.rke_network.id
+}
+
+resource "google_compute_address" "rkeinternaladdress" {
+  name         = "rkeinternaladdress"
+  subnetwork   = google_compute_subnetwork.rke_subnet.id
+  address_type = "INTERNAL"
+  address      = "10.0.0.2"
+  region       = var.gcp_region
+}
+
+resource "google_compute_address" "rkeexternaladdress" {
+  name = "rkeexternaladdress"
+	address_type = "INTERNAL"
+  address      = "35.224.234.6"
+  region       = var.gcp_region
+}
+
+
+# ********** WARNING **********
+# Firewall Rule to allow all traffic
+resource "google_compute_firewall" "rke_fw_allowall" {
+  name    = "${var.prefix}rke-allowall"
+  network = google_compute_network.rke_network.id
+
+  allow {
+    protocol = "all"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# disk
+resource "google_compute_disk" "rke_master_disk" {
+  name  = "master-disk"
+  image = data.google_compute_image.rke_master_image.self_link
+  size  = 30
+  type  = "pd-ssd"
+  zone  = var.gcp_zone
+  labels = {
+    env = "dev"
+  }
+}
+
+# GCP Compute Instance for creating a single node RKE cluster and installing the Rancher server
+resource "google_compute_instance" "rke_server" {
+  depends_on = [
+    google_compute_firewall.rke_fw_allowall,
+  ]
+
+  name         = "${var.prefix}master"
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
+  tags         = ["type", "terraform"]
+  labels = {
+    env = "dev"
+  }
+
+  # if need preemptible VM (24h)
+  # preemptible = true
 
   boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
+    # initialize_params {
+    #   image = data.google_compute_image.rke_master_image.self_link
+    # }
+    source = google_compute_disk.rke_master_disk.id
   }
 
   network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
+    network = google_compute_network.rke_network.id
     access_config {
+      nat_ip = google_compute_address.rkeexternaladdress.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${tls_private_key.global_key.public_key_openssh}"
+    user-data = templatefile(
+      join("/", [path.module, "userdata_rancher_server.template"]),
+      {
+        docker_version = var.docker_version
+        username       = local.node_username
+      }
+    )
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Waiting for cloud-init to complete...'",
+      "cloud-init status --wait > /dev/null",
+      "echo 'Completed cloud-init!'",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.network_interface.0.access_config.0.nat_ip
+      user        = local.node_username
+      private_key = tls_private_key.global_key.private_key_pem
     }
   }
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}
+
+
+
+
+# simple test
+# provider "google" {
+#   project = "kubernetes-292714" # PROJECT_ID
+#   region  = var.gcp_region
+#   zone    = "us-central1-f"
+# }
+
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "kubemaster"
+#   machine_type = "n1-standard-1"
+
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
+
+#   network_interface {
+#     # A default network is created for all GCP projects
+#     network = google_compute_network.vpc_network.self_link
+#     access_config {
+#     }
+#   }
+# }
+
+# resource "google_compute_network" "vpc_network" {
+#   name                    = "terraform-network"
+#   auto_create_subnetworks = "true"
+# }
